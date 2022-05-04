621f75f975cd0b1281fa56aa8a419bce40420281
Diaz <fdiaz-2017264@kinal.edu.gt>
2022-05-04 09:59:27 -0600

    Se creó el modelo User

---
+++ server/src/model/user.model.js
@@ -0,0 +1,16 @@
+'user strict'
+
+const mongoose = require('mongoose');
+
+
+const userSchema = mongoose.Schema({
+
+    name: String,
+    username: String,
+    email: String,
+    password: String,
+    role: String
+})
+
+module.exports = mongoose.model(userSchema, 'User');
+
