e0b8147952a9f75186c0a1f10cbc8af1a813f36a
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-05-08 19:56:27 -0600

    Se agregaron las rutas al servidor

--- server/configs/app.js
+++ server/configs/app.js
@@ -8,6 +8,7 @@ const helmet = require('helmet');
 const userRoutes = require('../src/routes/user.routes');
 const tournamentRoutes = require('../src/routes/tournament.routes');
 const leagueRoutes = require('../src/routes/league.routes');
+const teamRoutes = require('../src/routes/team.routes');
 
 
 const app = express(); 
@@ -20,6 +21,7 @@ app.use(cors());
 app.use('/user', userRoutes);
 app.use('/tour', tournamentRoutes);
 app.use('/league', leagueRoutes);
+app.use('/team', teamRoutes)
 
 
 
