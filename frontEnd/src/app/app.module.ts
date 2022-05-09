74f414e3f9c5dfff476b54c2344d771eef48dc23
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-05-09 10:19:15 -0600

    Se agregaron los componentes home, login, not found, register

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    LoginComponent,
+    NotFoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
