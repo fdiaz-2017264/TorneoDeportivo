e866ebb6301bc52d9011cfb2549f18982e63a147
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-05-08 19:20:41 -0600

    se agrego la ruta de create Score

--- server/src/routes/league.routes.js
+++ server/src/routes/league.routes.js
@@ -3,10 +3,12 @@
 const leagueController = require('../controller/league.controller');
 const express = require('express');
 const api = express.Router();
+const mdAuth = require('../services/authenticated');
 
 api.get('/testLeague', leagueController.testLeague);
 api.post('/saveLeague', mdAuth.ensureAuth,  leagueController.saveLeague);
 api.delete('/deleteLeague/:id', mdAuth.ensureAuth, leagueController.deleteLeague);
 api.put('/updatedLeague/:id', mdAuth.ensureAuth, leagueController.updatedLeague);
+api.put('/createScore/:id', mdAuth.ensureAuth, leagueController.createScore);
 
 module.exports = api;
